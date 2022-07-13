838de27279ddfb3ba4a169b1560e827c5523c033
PetrNiki <nikitenkopv@yandex.ru>
2022-07-13 13:45:27 +0300

    Задача1

---
+++ Задача1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите число");
+int nam = int.Parse(Console.ReadLine());
+int nam1 = (nam / 10) % 10;
+Console.WriteLine(nam1);
